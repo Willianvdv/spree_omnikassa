125afd77b23716ed6c7ed4176703ad96b533e3a8
Willian <willian@pythonheads.nl>
2012-10-25 23:26:10 +0200

    Confirm step

--- app/models/spree/billing_integration/omnikassa.rb
+++ app/models/spree/billing_integration/omnikassa.rb
@@ -4,10 +4,27 @@ module Spree
     preference :secret_key, :string
     preference :key_version, :string
 
+    def payment_profiles_supported?
+        true
+    end
+
     attr_accessible :preferred_server,
                     :preferred_test_mode,
                     :preferred_merchant_id, 
                     :preferred_secret_key, 
                     :preferred_key_version
+
+    def provider_class
+      puts 'PROVIDER CLASS'
+      ActiveMerchant::Billing::Omnikassa 
+    end
+
+    #def redirect_url order, opts = {}
+    #  'http://www.google.nl'
+    #end
+
+    #def payment_source_class
+    #  nil #Spree::Omnikassa
+    #end
   end
 end
--- app/models/spree/omnikassa.rb
+++ app/models/spree/omnikassa.rb
@@ -1,6 +1,5 @@
 module Spree
   class Omnikassa < ActiveRecord::Base
     has_many :payments, :as => :source
-
   end
 end
