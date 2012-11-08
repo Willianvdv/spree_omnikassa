34cc8d13e4e81ca65d87c1e4959c4371b7f4c7a7
Willian <willian@pythonheads.nl>
2012-11-08 19:19:00 +0100

    Using money lib for omnikassa payment

--- lib/spree/omnikassa_payment.rb
+++ lib/spree/omnikassa_payment.rb
@@ -1,11 +1,26 @@
+require 'money'
+
 module Spree
   class OmnikassaPayment
-    def initialize order
+    def initialize(order)
       @order = order
     end
 
+    def money
+      ::Money.parse([@order.amount, Spree::Config[:currency]].join)
+    end
+
+    def amount
+      money.cents() 
+    end
+
+    def currencyCode
+      money.currency.iso_numeric
+    end
+    
     def payment_data
-      
+      {:amount => amount,
+       :currencyCode => currencyCode}
     end 
   end
 end
--- spec/lib/omnikassa_payment_spec.rb
+++ spec/lib/omnikassa_payment_spec.rb
@@ -1,17 +1,21 @@
 require 'spec_helper'
-require './lib/spree/omnikassa_payment'
 
 module Spree
   describe OmnikassaPayment do
     let(:order) do
-      FactoryGirl.create :order
+      FactoryGirl.create :completed_order_with_totals
     end
 
-    subject { Spree::OmnikassaPayment order }
+    subject { Spree::OmnikassaPayment.new(order) }
 
     context 'payment data' do
-      it 'has the right amount' do
-        puts subject.payment_data
+      it 'has the amount in cents' do
+        subject.payment_data[:amount].should equal 1000
+      end
+
+      it 'has has the currency code of the current configuration' do
+        # HARDCODE CURRENCY CODE!!!!
+        subject.payment_data[:currencyCode].should eq '840' #840 = $, 978 = €
       end
     end
   end
