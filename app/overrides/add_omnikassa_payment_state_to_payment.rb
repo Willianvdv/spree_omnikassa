4254e3b832a685b1544601664806898ccaa3e256
Willian <willian@pythonheads.nl>
2012-11-19 16:19:41 +0100

    Added response code to payment list

---
+++ app/overrides/add_omnikassa_payment_state_to_payment.rb
@@ -0,0 +1,10 @@
+Deface::Override.new(:virtual_path => "spree/admin/payments/_list",
+                     :name => "add_omnikassa_payment_state_to_payment_head",
+                     :insert_bottom => '[data-hook="payments_header"]',
+                     :text => '<th>Response code</th>')
+
+Deface::Override.new(:virtual_path => "spree/admin/payments/_list",
+                     :name => "add_omnikassa_payment_state_to_payment_body",
+                     :insert_bottom => '[data-hook="payments_row"]',
+                     :text => '<td><% if payment.payment_method.class == Spree::BillingIntegration::Omnikassa %><%= payment.omnikassa_payment.omnikassa_response_code %><% end %></td>')
+
