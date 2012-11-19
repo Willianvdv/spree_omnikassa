97c728ef71fbec1ba5b6d25e55fabe725af217af
Willian <willian@pythonheads.nl>
2012-11-19 17:59:57 +0100

    Added settings to index

--- app/overrides/add_omnikassa_settings_to_configuration_tab.rb
+++ app/overrides/add_omnikassa_settings_to_configuration_tab.rb
@@ -1,4 +1,13 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
-                     :name => "add_omnikassa_settings_to_configuration_menu",
-                     :insert_bottom => '[data-hook="admin_configurations_sidebar_menu"]',
-                     :text => '<%= configurations_sidebar_menu_item t(:omnikassa_settings), admin_omnikassa_settings_path %>')
+#Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
+#                     :name => "add_omnikassa_settings_to_configuration_menu",
+#                     :insert_bottom => '[data-hook="admin_configurations_sidebar_menu"]',
+#                     :text => '<%= configurations_sidebar_menu_item t(:omnikassa_settings), edit_admin_omnikassa_settings %>')
+
+Deface::Override.new(:virtual_path => "spree/admin/configurations/index",
+                     :name => "add_omnikassa_settings_to_configuration_index",
+                     :insert_bottom => '[data-hook="admin_configurations_menu"]',
+                     :text => '<tr>
+                                 <td><%= link_to t(:omnikassa_settings), :edit_admin_omnikassa_settings %></td>
+                                 <td><%= t(:omnikassa_settings) %></td>
+                               </tr>')
+
