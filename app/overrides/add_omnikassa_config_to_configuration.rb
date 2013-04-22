60c613e56dcdf2869f0c2f5d9a6034d9553ad356
Willian van der Velde <willian@pythonheads.nl>
2013-04-22 11:33:59 +0200

    Enabled omnikassa settings tab

--- app/overrides/add_omnikassa_config_to_configuration.rb
+++ app/overrides/add_omnikassa_config_to_configuration.rb
@@ -1,4 +1,4 @@
-#Deface::Override.new(:virtual_path => "spree/admin/general_settings/edit",
-#                     :name => "add_merchant_preference_to_preferences",
-#                     :insert_bottom => '[data-hook="preferences"]',
-#                     :partial => 'spiree/admin/')
+# Deface::Override.new(:virtual_path => "spree/admin/general_settings/edit",
+#                      :name => "add_merchant_preference_to_preferences",
+#                      :insert_bottom => '[data-hook="preferences"]',
+#                      :partial => 'spree/admin/')
