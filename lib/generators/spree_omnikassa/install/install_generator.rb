cc6a1da4f6ae2d989c9e88d603ad9efd380835de
Willian van der Velde <willian@pythonheads.nl>
2014-02-25 16:36:34 +0100

    Bump to 2-2

--- lib/generators/spree_omnikassa/install/install_generator.rb
+++ lib/generators/spree_omnikassa/install/install_generator.rb
@@ -3,17 +3,6 @@ module SpreeOmnikassa
     class InstallGenerator < Rails::Generators::Base
       class_option :auto_run_migrations, :type => :boolean, :default => false
 
-
-      def add_javascripts
-        append_file 'app/assets/javascripts/store/all.js', "//= require store/spree_omnikassa\n"
-        append_file 'app/assets/javascripts/admin/all.js', "//= require admin/spree_omnikassa\n"
-      end
-
-      def add_stylesheets
-        inject_into_file 'app/assets/stylesheets/store/all.css', " *= require store/spree_omnikassa\n", :before => /\*\//, :verbose => true
-        inject_into_file 'app/assets/stylesheets/admin/all.css', " *= require admin/spree_omnikassa\n", :before => /\*\//, :verbose => true
-      end
-
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_omnikassa'
       end
