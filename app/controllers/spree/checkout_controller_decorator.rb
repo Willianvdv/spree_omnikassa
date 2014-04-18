4e6b7c9aea486d2d0fdf0c02d09b37267f78b23a
Willian van der Velde <willian@pythonheads.nl>
2014-04-18 12:39:37 +0200

    Completion route fix

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,22 +1,24 @@
 Spree::CheckoutController.class_eval do
   private
-    def completion_route
-      if is_omnikassa_payment?
-        url_for :controller => 'omnikassa',
-                :action => 'start',
-                :payment_id => payment.id,
-                :token => @order.token
-      else
-        super
-      end
-    end
 
-    def payment
-      @order.payments.first
+  def completion_route
+    if is_omnikassa_payment?
+      url_for :controller => 'omnikassa',
+              :action => 'start',
+              :payment_id => payment.id,
+              :token => @order.token
+    else
+      # This is the original completion route
+      spree.order_path(@order)
     end
+  end
 
-    def is_omnikassa_payment?
-      payment_method = @order.payments.last.payment_method
-      payment_method.class == Spree::BillingIntegration::Omnikassa
-    end
+  def payment
+    @order.payments.first
+  end
+
+  def is_omnikassa_payment?
+    payment_method = @order.payments.last.payment_method
+    payment_method.class == Spree::BillingIntegration::Omnikassa
+  end
 end
