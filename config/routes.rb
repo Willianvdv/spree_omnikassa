aff58d3ad1700f8f9c19b36f52ce401039be5395
Willian van der Velde <mail@willian.io>
2014-10-24 16:02:03 +0200

    Use access token for guest access

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,22 @@
 Spree::Core::Engine.add_routes do
-  
+
   get '/omnikassa/start/:payment_id/',
       :to => 'omnikassa#start',
       :as => :omnikassa_start
   get '/omnikassa/error/:payment_id/',
       :to => 'omnikassa#error',
       :as => :omnikassa_error
-  get '/omnikassa/restart/:order_id/',
-      :to => 'omnikassa#restart',
-      :as => :omnikassa_restart
+
   post '/omnikassa/success/:payment_id/',
       :to => 'omnikassa#success',
       :as => :omnikassa_success
   post '/omnikassa/success/automatic/:payment_id/',
       :to => 'omnikassa#success_automatic',
       :as => :omnikassa_success_automatic
+
+  get '/omnikassa/restart/:order_id/',
+      :to => 'omnikassa#restart',
+      :as => :omnikassa_restart
 
   namespace :admin do
     resource :omnikassa_settings
