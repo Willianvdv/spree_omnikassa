4ed215cebc024017600a5b700c7aa9632e01032e
Rein Aris <info@reinaris.nl>
2016-10-17 12:26:04 +0200

    added customer language setting

--- lib/spree/omnikassa_configuration.rb
+++ lib/spree/omnikassa_configuration.rb
@@ -1,6 +1,7 @@
 class Spree::OmnikassaConfiguration < Spree::Preferences::Configuration
   preference :omnikassa_merchant_id, :string, :default => '002020000000001'
   preference :omnikassa_secret_key, :string, :default => '002020000000001_KEY1'
-  preference :omnikassa_key_version, :string, :default => '1' 
+  preference :omnikassa_key_version, :string, :default => '1'
   preference :omnikassa_transaction_reference_prefix, :string, :default => 'PREFIX'
+  preference :omnikassa_customer_language, :string, :default => 'NL'
 end
